3ff3e8a95e3c4d09be629628f01ff84a11922d89
olivejp <jean-paul.olive@deveo.nc>
2022-08-07 15:41:13 +1100

    CI commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCh1JXX3tFZs3LKBxSxd8mCx-bIfCHvNCM",
+    authDomain: "fitnc-7be2e.firebaseapp.com",
+    projectId: "fitnc-7be2e",
+    storageBucket: "fitnc-7be2e.appspot.com",
+    messagingSenderId: "426590177970",
+    appId: "1:426590177970:web:f4281f086a8e8326ff17ab",
+    measurementId: "G-MV0F605LW5"
+  }
 };
